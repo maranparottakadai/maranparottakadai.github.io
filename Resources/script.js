b108b7a282cd404d0d630df659382262179e6a8c
maranparottakadai <maranparottakadai4lyf@gmail.com>
2025-03-29 10:29:46 +0530

    Update script.js

--- Resources/script.js
+++ Resources/script.js
@@ -178,18 +178,3 @@ async function initializeApp() {
       });
   }
 } 
-
-
-fetch("./Resources/r_index.json")
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return response.json();
-  })
-  .then((data) => {
-    populateContent(data);
-  })
-  .catch((error) => {
-    console.error("Error loading index.json:", error);
-  });
